533b98ad317f36a818398f7aefb3c10217cc30d3
jessalsua <jsalas@us.es>
2025-05-22 15:48:35 +0200

    test

---
+++ test.py
@@ -0,0 +1,10 @@
+import os
+import sys
+
+# Add OceanDirect path
+sys.path.append('/usr/local/OceanOptics/OceanDirect/python')
+
+from oceandirect import OceanDirectAPI as od
+import numpy as np
+
+
